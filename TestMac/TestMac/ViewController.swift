9c6b823afee904179fe8df1be96259b6fe7390a9
Entao <entaoyang@163.com>
2021-02-13 18:44:15 +0800

    0.1.7

--- TestMac/TestMac/ViewController.swift
+++ TestMac/TestMac/ViewController.swift
@@ -19,44 +19,26 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        let styleText = styledText {
-//            $0.append("a", .foregroundColor, Color.blue)
-//            $0.append("b", .backgroundColor, Color.white)
-//        }
 
-
-        func label(_ n: Int) -> NSTextField {
-            let lb = NSTextField(labelWithString: "Hello \(n)")
-            lb.backColor(.red)
-            return lb
+        view += LinearLayout(.horizontal).constraints { c in
+            c.centerParent()
+            c.widthParent(multi: 1, constant: -10)
+            c.heightParent(multi: 1, constant: -10)
+        }.apply { ll in
+//            ll.backColor(.green)
+        }.buildViews {
+            NSTextField(labelWithString: "HaHa").linearParams { p in
+                p.weight = 1
+                p.width = MatchParent
+                p.gravityX = .center
+                p.gravityY = .center
+            }.apply { v in
+                v.backColor(.green)
+                v.textColor = .red
+                v.alignment = .center
+            }
         }
 
-//        let ll = LinearLayout(NSLayoutConstraint.Orientation.vertical)
-        view += LinearLayout(.horizontal).apply { ll in
-            ll.constraints { c in
-                c.centerParent()
-                c.widthParent(multi: 1, constant: -10)
-                c.heightParent(multi: 1, constant: -10)
-            }
-            ll.backColor(.green)
-            for i in 1 ... 20 {
-                ll += label(i).linearParams { p in
-//                    p.width = 0
-//                    p.height = 0
-                    p.weight = 1
-//                    p.gravityX = .fill
-//                    p.gravityY = .fill
-                    p.margins.ver(5).hor(5)
-                }
-            }
-
-        }
-
-//        view += lb.constraints { c in
-//            c.centerParent()
-//            c.widthParent(multi: 1, constant: -20)
-//            c.heightParent(multi: 1, constant: -40)
-//        }
     }
 
 
